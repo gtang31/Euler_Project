c1923bc6ba12a633a532a009717ebbe332f71f20
gtang0131 <garytang@quadanalytix.com>
2015-01-20 23:33:36 -0800

    Answered Proj17

--- Proj17_LetterCount.py
+++ Proj17_LetterCount.py
@@ -16,70 +16,72 @@ NOTE: Do not count spaces or hyphens. For example, 342
 British usage.
 """
 
-# this project makes use of Dictionaries and Split function
-"""
-track = {   'one':3
-            'two':3
-            'three':5
-            'four':4
-            'five':4
-            'six':3
-            'seven':5
-            'eight':5
-            'nine':4
-            'ten':3
-            'eleven':6
-            'twelve':6
-            'thirteen':8
-            'fourteen':8
-            'fifteen':7
-            'sixteen':7
-            'seventeen':9
-            'eighteen':8
-            'nineteen':8
-            'twenty':6
-            'thirty':6
-            'forty':5
-            'fifty':5
-            'sixty':5
-            'seventy':7
-            'eighty':6
-            'ninety':6
-            'hundred':7
-            'thousand':8
-        }
-"""
-track = {   1:3
-            2:3
-            3:5
-            4:4
-            5:4
-            6:3
-            7:5
-            8:5
-            9:4
-            10:3
-            11:6
-            12:6
-            13:8
-            14:8
-            15:7
-            16:7
-            17:9
-            18:8
-            19:8
-            20:6
-            30:6
-            40:5
-            50:5
-            60:5
-            70:7
-            80:6
-            90:6
-            100:7
-            1000:8
-        }
-def CountLetters(track):
-    for i in range(1,1001):
-        
-    
+ones_track = {
+                '0'   :0,
+                '1'   :3,
+                '2'   :3,
+                '3'   :5,
+                '4'   :4,
+                '5'   :4,
+                '6'   :3,
+                '7'   :5,
+                '8'   :5,
+                '9'   :4,
+                'and' :3}
+teens_track = {
+                '10'  :3,
+                '11'  :6,
+                '12'  :6,
+                '13'  :8,
+                '14'  :8,
+                '15'  :7,
+                '16'  :7,
+                '17'  :9,
+                '18'  :8,
+                '19'  :8}
+
+tens_track = {
+                '0'  :0,
+                '2'  :6,
+                '3'  :6,
+                '4'  :5,
+                '5'  :5,
+                '6'  :5,
+                '7'  :7,
+                '8'  :6,
+                '9'  :6,
+                'hundred' :7,
+                'thousand':8}
+
+def CountLetters(ones_track, teens_track, tens_track):
+    letter_count = 0
+
+    for i in xrange(1,1000):
+        #print i
+        current_number = list(str(i))
+
+        if len(current_number) == 1:
+            letter_count += ones_track[str(i)]
+
+        elif len(current_number) >= 2:
+            #import pdb; pdb.set_trace()
+            if int(''.join(current_number[-2:])) in range(10,20):
+                letter_count += teens_track[''.join(current_number[-2:])]
+
+            else:
+                #import pdb; pdb.set_trace()
+                letter_count += tens_track[current_number[-2]]
+                letter_count += ones_track[current_number[-1]]
+
+            if len(current_number) > 2:
+                letter_count += ones_track[current_number[-3]]
+                letter_count += tens_track['hundred']
+                if ''.join(current_number[-2:]) != '00':
+                    letter_count += ones_track['and']
+
+    letter_count += tens_track['thousand']
+    letter_count += ones_track['1']
+
+    print letter_count
+
+CountLetters(ones_track, teens_track, tens_track)
